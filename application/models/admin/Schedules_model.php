c7f4ed27f0b0419b6081913e8c9bbab5a4dc60a0
kimtoledo <kma.toledo@gmail.com>
2019-02-03 00:31:37 +0800

    [Update] - Update Admin Calendar schedule

--- application/models/admin/Schedules_model.php
+++ application/models/admin/Schedules_model.php
@@ -1,5 +1,6 @@
 <?php
 class Schedules_model extends CI_Model{
+    
     public function getSchedules($status = null)
     {   
         $this->db->select('*');
@@ -14,6 +15,22 @@ class Schedules_model extends CI_Model{
         $res = $this->db->get()->result();
         return $res;
     }
+
+    public function getSchedule($id)
+    {   
+        $this->db->select('*');
+        $this->db->from('schedules');
+        $this->db->join('profiles', 'profiles.user_id = schedules.user_id');
+        $this->db->join('pets', 'pets.pet_id = schedules.pet_id');
+        $this->db->join('pet_types', 'pet_types.pet_type_id = pets.pet_type_id');
+        $this->db->join('schedule_status', 'schedules.status = schedule_status.id');
+        $this->db->where('schedules.is_deleted', 0);
+        $this->db->where('schedules.schedule_id', $id);
+        $res = $this->db->get()->result();
+        return $res;
+    }
+
+
      public function getAllScheduleCalendar(){
         $query = $this->db->query("
             SELECT 
